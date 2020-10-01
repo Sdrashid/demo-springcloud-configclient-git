7674c1ccd5aaddd545fff1e4167b123db54075f2
mohammad.saiyed <Mohammad.Saiyed@eclinicalworks.com>
2020-10-01 05:20:56 -0400

    Initial commit

---
+++ src/test/java/com/techtalk/demo/DemoSpringcloudConfigclientGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtalk.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringcloudConfigclientGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
